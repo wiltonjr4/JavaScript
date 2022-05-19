aee4abec6ea98dea644128336dd8c1cbf90f1b90
Wilton Junior <wilton.jr66@gmail.com>
2022-05-19 11:19:12 -0300

    CRUD Update
    Ends the CRUD in the correct folders

--- NodeJS_Studies/Bookstore_API/src/app.js
+++ NodeJS_Studies/Bookstore_API/src/app.js
@@ -1,6 +1,5 @@
 import express from "express";
 import db from "./config/dbConnect.js"
-import books from "./models/Book.js"
 import routes from "./routes/index.js"
 
 db.on("error", console.log.bind(console, 'Connection Error'))
@@ -9,34 +8,7 @@ db.once("open", () => {
 })
 
 const app = express();
-
 app.use(express.json());
-
 routes(app);
-
-
-app.get('/books/:id', (req, res) => {
-    let index = getBook(req.params.id);
-    res.json(books[index]);
-})
-
-
-app.put('/books/:id', (req, res) => {
-    let index = getBook(req.params.id);
-    books[index].title = req.body.title;
-    res.json(books);
-})
-
-app.delete('/books/:id', (req, res) => {
-    let {id} = req.params;
-    let index = getBook(id);
-    books.splice(index, 1);
-    res.send(`Book ${id} successfully deleted`);
-})
-
-
-function getBook(id){
-    return books.findIndex(books => books.id == id)
-}
 
 export default app;
--- NodeJS_Studies/Bookstore_API/src/controllers/booksController.js
+++ NodeJS_Studies/Bookstore_API/src/controllers/booksController.js
@@ -8,6 +8,18 @@ class BookController {
         })
     }
 
+    static listBookById = (req, res) => {
+        const id = req.params.id;
+
+        books.findById(id, (err, books) => {
+            if(err){
+                res.status(400).send({message: `${err.message} - Can't find this book ID`})
+            } else {
+                res.status(200).send(books);
+            }
+        })
+    }
+
     static registerBook = (req, res) => {
         let book = new books(req.body);
 
@@ -20,6 +32,31 @@ class BookController {
             }
         })
     }
+
+    static updateBook = (req, res) => {
+        const id = req.params.id;
+
+        books.findByIdAndUpdate(id, {$set: req.body}, (err) => {
+            if(!err){
+                res.status(200).send({message: 'Book successfully updated!'})
+            } else {
+                res.status(500).send({message: err.message})
+            }
+        })
+    }
+
+    static deleteBook = (req, res) => {
+        const id = req.params.id;
+
+        books.findByIdAndDelete(id, (err) => {
+            if(!err){
+                res.status(200).send({message: 'Book successfully deleted!'})
+            } else {
+                res.status(500).send({message: err.message})
+            }
+        })
+    }
+    
 }
 
 export default BookController;
